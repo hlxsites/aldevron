db291870e09e944213cae71287cb8dcb51cba10b
Pardeep Gera <pardeep.gera@MACPRO484.local>
2023-10-13 09:19:31 +0530

    Included the gtm script

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,3 +5,35 @@ import { sampleRUM } from './aem.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
+
+// google tag manager -start
+function loadGTM() {
+    const scriptTag = document.createElement('script');
+    scriptTag.innerHTML = `
+        let gtmId = 'GTM-MLWV3QQ';
+        // googleTagManager
+        (function (w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start':
+                    new Date().getTime(), event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', gtmId);
+        `;
+    document.head.prepend(scriptTag);
+}
+// google tag manager -start
+
+if (
+    !window.location.hostname.includes('localhost')
+    && !document.location.hostname.includes('.hlx.page')
+) {
+    loadGTM();
+}
+
