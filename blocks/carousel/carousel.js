937bca5153c8b8a311c071da7894f4319226dae0
umamenda <umamaheswara.menda@gspann.com>
2023-10-18 20:20:51 +0530

    rendering the carousel content from document

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,26 +1,88 @@
-import { readBlockConfig, decorateIcons } from '../../scripts/aem.js';
-/**
- * loads and decorates the carousel
- * @param {Element} block The carousel block element
- */
 export default async function decorate(block) {
-  const cfg = readBlockConfig(block);
-  block.textContent = '';
   // fetch carousel content
-  const carouselPath = cfg.carousel || '/carousel';
-  const resp = await fetch(`${carouselPath}.plain.html`, window.location.pathname.endsWith('/carousel') ? { cache: 'reload' } : {});
-  function createDivElement(className, IDName) {
-    const divEl = document.createElement('div');
-    divEl.setAttribute('class', className);
-    divEl.setAttribute('id', IDName);
-    return divEl;
+  // function createDivElement(className, IDName) {
+  //   const divEl = document.createElement('div');
+  //   divEl.setAttribute('class', className);
+  //   divEl.setAttribute('id', IDName);
+  //   return divEl;
+  // }
+  var slideIndex = 1;
+  var myTimer;
+  var slideshowContainer;
+  window.addEventListener("load", function () {
+    showSlides(slideIndex);
+    myTimer = setInterval(function () {
+      plusSlides(1);
+    }, 4000);
+    //COMMENT OUT THE LINE BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+    slideshowContainer = document.getElementsByClassName("slideshow-inner")[0];
+    //UNCOMMENT OUT THE LINE BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+    // slideshowContainer = document.getElementsByClassName('slideshow-container')[0];
+    if(slideshowContainer) {
+      slideshowContainer.addEventListener("mouseenter", pause);
+      slideshowContainer.addEventListener("mouseleave", resume);
+    }
+  });
+  // NEXT AND PREVIOUS CONTROL
+  function plusSlides(n) {
+    clearInterval(myTimer);
+    if (n < 0) {
+      showSlides((slideIndex -= 1));
+    } else {
+      showSlides((slideIndex += 1));
+    }
+    //COMMENT OUT THE LINES BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+    if (n === -1) {
+      myTimer = setInterval(function () {
+        plusSlides(n + 2);
+      }, 4000);
+    } else {
+      myTimer = setInterval(function () {
+        plusSlides(n + 1);
+      }, 4000);
+    }
   }
-  if (resp.ok) {
-    const html = await resp.text();
-    const topContainer = createDivElement('top-container', '');
-    const carouselWapper = createDivElement('carousel', 'hs_menu_wrapper_carousel_nav');
-    carouselWapper.innerHTML = html;
-    decorateIcons(carouselWapper);
-    block.append(topContainer);
+  //Controls the current slide and resets interval if needed
+  function currentSlide(n) {
+    clearInterval(myTimer);
+    myTimer = setInterval(function () {
+      plusSlides(n + 1);
+    }, 4000);
+    showSlides((slideIndex = n));
   }
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if(slides.length) {
+      var dots = document.getElementsByClassName("dot");
+      if (n > slides.length) {
+        slideIndex = 1;
+      }
+      if (n < 1) {
+        slideIndex = slides.length;
+      }
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        // slides[slideIndex - 1].className.replace('slide', '');
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex - 1].style.display = "block";
+      setInterval(function () {
+        slides[slideIndex - 1].classList.add('slide');
+      }, 1000)
+      slides[slideIndex - 1].classList.add('slide');
+      dots[slideIndex - 1].className += " active";
+    }
+  }
+
+  // Use constants and destructuring to make code cleaner
+  const carouselWrapper = document.querySelector(".carousel-wrapper");
+  const cards = document.querySelectorAll(".carousel-wrapper-card");
+  const cardWidth = cards[0] ? cards[0].offsetWidth + parseInt(getComputedStyle(cards[0]).marginRight): 0;
+  const dotsContainer = document.querySelector(".carousel-dots-container");
+  // const prevButton = document.querySelector(".prev-button");
+  // const nextButton = document.querySelector(".next-button");
+  const dots = [];
 }
