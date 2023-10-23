e6f85ba64f5f8f02453d45c408f71cbc7b822f58
Gowtami <gsp24112@adobe.com>
2023-10-23 18:46:17 +0530

    fixed lint issues

--- blocks/contactus/contactus.js
+++ blocks/contactus/contactus.js
@@ -6,17 +6,17 @@ export default function decorate() {
     const contactUsDiv = contactUsClass[0].children[0].children[0];
     const pTags = contactUsDiv.getElementsByTagName('p');
     let innerElements = '';
-    let phone ='';
+    let phone = '';
     for (let i = 0; i < pTags.length; i += 1) {
-        if (i === 0){
+        if (i === 0) {
             const ContactUsTitle = pTags[i].outerHTML.replace(/<p>/g, '<h2>');
             innerElements += ContactUsTitle.replace(/<\/p>/g, '</h2>');
-        }else  if (i === 1){
+        } else  if (i === 1) {
             innerElements +=pTags[i].outerHTML;
-        }else  if(i === 3){
+        } else  if (i === 3) {
             phone = pTags[2].outerHTML.replace(/<p>/g, '<a class="phone" href="tel:' +pTags[2].innerHTML.trim()+ '">');
             phone.replace(/<\/p>/g, '</a>');
-            pTags[i].innerHTML =  '<div class="buttons">' + phone+pTags[i].innerHTML +'</div>';
+            pTags[i].innerHTML =  '<div class="buttons">' + phone + pTags[i].innerHTML +'</div>';
             const button = pTags[i].outerHTML.replace(/<p>/g, '');
             innerElements += button.replace(/<\/p>/g, '');
         }
