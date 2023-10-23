a352a9f3813915e4794f20cbb1ccc3f0513706f8
vinaykumargujja <vinay.gujja@gspann.com>
2023-10-23 11:20:12 +0530

    images with and without hyperlink

---
+++ blocks/imageshyperlink/imageshyperlink.js
@@ -0,0 +1,21 @@
+import { readBlockConfig } from '../../scripts/aem.js';
+/**
+ * loads and decorates the footer
+ * @param {Element} block The footer block element
+ */
+export default async function decorate(block) {
+  const anchorEl = document.createElement("a");
+  let refAnchorEl = block.querySelector("a");
+  let pic = block.querySelector("picture");
+  if (refAnchorEl) {
+    refAnchorEl.hasAttribute("href") && anchorEl.setAttribute("href", refAnchorEl.getAttribute("href"));
+    refAnchorEl.getAttribute("title") && anchorEl.setAttribute("title", refAnchorEl.getAttribute("title"));
+    refAnchorEl.getAttribute("alt") && anchorEl.setAttribute("alt", refAnchorEl.getAttribute("alt"));
+    anchorEl.appendChild(pic);
+    block.textContent = "";
+    block.append(anchorEl);
+  } else {
+    block.textContent = "";
+    block.append(pic);
+  }
+}
