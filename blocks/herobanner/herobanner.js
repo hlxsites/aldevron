6bd9493c32186b4643c51e236ebfaa84aeec6f83
vinaykumargujja <vinay.gujja@gspann.com>
2023-10-23 19:51:16 +0530

    updated with herobanner code

---
+++ blocks/herobanner/herobanner.js
@@ -0,0 +1,26 @@
+import { div, h3, p } from '../../scripts/dom-builder.js';
+
+export default function decorate(block) {
+    const clonedBlock = block.cloneNode(true);
+    const pictureElement = clonedBlock.querySelector('picture');
+    const anchorButton = clonedBlock.querySelector('p.button-container');
+    const heroBanner = div(
+        { class: 'hero-banner right-content' },
+        pictureElement
+    );
+    const contentEl = div({ class: 'content' });
+    console.log(anchorButton.previousElementSibling && anchorButton.previousElementSibling.textContent != '');
+    if (anchorButton.previousElementSibling && anchorButton.previousElementSibling.previousElementSibling && anchorButton.previousElementSibling.previousElementSibling.textContent != '') {
+        const title = h3({ class: 'title' }, anchorButton.previousElementSibling.previousElementSibling.textContent);
+        contentEl.append(title);
+    }
+    if (anchorButton.previousElementSibling && anchorButton.previousElementSibling.textContent != '') {
+        const description = p({ class: 'description' }, anchorButton.previousElementSibling.textContent);
+        contentEl.append(description);
+    }
+    if (anchorButton.children.length > 0) contentEl.append(anchorButton.children[0]);
+    const arrow = div({ class: 'arrow' });
+    heroBanner.append(contentEl);
+    heroBanner.prepend(arrow);
+    block.replaceChildren(heroBanner);
+}
