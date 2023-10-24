bfd38cedefbae2d0e48fb1ca550571d61b4fb47b
RajSekhar <manuballa.rajsekhar@gspann.com>
2023-10-24 16:54:29 -0500

    Image Popup changes

--- blocks/imgpopup/imgpopup.js
+++ blocks/imgpopup/imgpopup.js
@@ -2,37 +2,36 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 import { div, p } from '../../scripts/dom-builder.js';
 import { loadScript } from '../../scripts/scripts.js';
 
-
 export default function decorate(block) {
 
     loadScript('../../scripts/jquery-1.11.2.js', { type: 'text/javascript', charset: 'UTF-8' })
-    .then(() => {
-      // these scripts depend on jquery:
-      loadScript('../../scripts/jquery.colorbox-min.js', { type: 'text/javascript', charset: 'UTF-8' })
-      .then(() => {
-        // these scripts depend on colorbox:
-        loadScript('../../scripts/custom.js', { type: 'text/javascript', charset: 'UTF-8' });
-      });
-      
-    });
+        .then(() => {
+            // these scripts depend on jquery:
+            loadScript('../../scripts/jquery.colorbox-min.js', { type: 'text/javascript', charset: 'UTF-8' })
+                .then(() => {
+                    // these scripts depend on colorbox:
+                    loadScript('../../scripts/custom.js', { type: 'text/javascript', charset: 'UTF-8' });
+                });
 
-    const pVar = p();
-    const imgDiv = div({ class: 'content flex cols2' },
-        div({ class: 'col' },
-        div({ class: 'text' },
-        pVar
-        ),
-        ),
-    );
+        });
 
+    
+    const flexEl = div({ class: 'content flex cols2' });
     [...block.children].forEach((row) => {
-        while (row.firstElementChild) pVar.append(row.firstElementChild);
+        const pEl = p();
+        const colEl = div({ class: 'col' },
+            div({ class: 'text' },
+                pEl
+            ),
+        );
+        while (row.firstElementChild) pEl.append(row.firstElementChild);
+        //colEl.querySelectorAll('img').forEach((img) => img.closest('div').classList.add('img-colorbox-popup', 'cboxElement'));
+        colEl.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{}])));
+        colEl.querySelectorAll('img').forEach((img) => img.src = img.src + '&popup');
+        flexEl.append(colEl);
     });
 
-    //imgDiv.querySelectorAll('img').forEach((img) => img.closest('div').classList.add('img-colorbox-popup', 'cboxElement'));
-    imgDiv.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ }])));
-    imgDiv.querySelectorAll('img').forEach((img) => img.src = img.src+'&popup');
     block.textContent = '';
-    block.append(imgDiv);
+    block.append(flexEl);
 }
 
