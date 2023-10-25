a7953805c31cfc3b07ceda18d211e9c2210d32fa
TeshuKatepalli <teshu.katepalli@gspann.com>
2023-10-25 16:52:18 +0530

    Updated the sidebar 2 variations

---
+++ blocks/sidebar-form/sidebar-form.js
@@ -0,0 +1,29 @@
+function embedHubSpotForm(formFields) {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = '/scripts/v2.js';
+    script.charset = 'utf-8';
+    script.setAttribute('async', '');
+    script.onload = () => {
+      window.hbspt.forms.create(formFields);
+    };
+    document.head.appendChild(script);
+  }
+  
+  export default function decorate(block) {
+    const formDetails = {};
+    const tableRows = block.querySelectorAll('tr');
+    tableRows.forEach((row) => {
+      const cells = row.children;
+      if (cells.length >= 2) {
+        const key = cells[0].innerText.trim();
+        const value = cells[1].innerText.trim();
+        formDetails[key] = key === 'target' ? `#${value}` : value;
+      }
+    });
+    embedHubSpotForm(formDetails);
+    const form = document.createElement('div');
+    form.id = formDetails.target.replace('#', '');
+    block.textContent = '';
+    block.append(form);
+  }
