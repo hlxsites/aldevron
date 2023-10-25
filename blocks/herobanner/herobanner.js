a1c06cc6eb905e64523b4fba7ffce9f174fb4984
vinaykumargujja <vinay.gujja@gspann.com>
2023-10-25 16:52:05 +0530

    updated with herobanner with css js

--- blocks/herobanner/herobanner.js
+++ blocks/herobanner/herobanner.js
@@ -1,26 +1,40 @@
-import { div, h3, p } from '../../scripts/dom-builder.js';
-
 export default function decorate(block) {
-    const clonedBlock = block.cloneNode(true);
-    const pictureElement = clonedBlock.querySelector('picture');
-    const anchorButton = clonedBlock.querySelector('p.button-container');
-    const heroBanner = div(
-        { class: 'hero-banner right-content' },
-        pictureElement
-    );
-    const contentEl = div({ class: 'content' });
-    console.log(anchorButton.previousElementSibling && anchorButton.previousElementSibling.textContent != '');
-    if (anchorButton.previousElementSibling && anchorButton.previousElementSibling.previousElementSibling && anchorButton.previousElementSibling.previousElementSibling.textContent != '') {
-        const title = h3({ class: 'title' }, anchorButton.previousElementSibling.previousElementSibling.textContent);
-        contentEl.append(title);
-    }
-    if (anchorButton.previousElementSibling && anchorButton.previousElementSibling.textContent != '') {
-        const description = p({ class: 'description' }, anchorButton.previousElementSibling.textContent);
-        contentEl.append(description);
-    }
-    if (anchorButton.children.length > 0) contentEl.append(anchorButton.children[0]);
-    const arrow = div({ class: 'arrow' });
-    heroBanner.append(contentEl);
-    heroBanner.prepend(arrow);
-    block.replaceChildren(heroBanner);
+  const clonedBlock = block.cloneNode(true);
+  const pictureElement = clonedBlock.querySelector('picture');
+  const moduleDiv = document.createElement('div');
+  moduleDiv.classList.add('module', 'mmg-rich-columns', 'padding-all', 'custom-bg', 'bg', 'wide-section', 'split-bg', 'style-standard')
+  moduleDiv.style.backgroundColor = '#fff';
+  moduleDiv.style.backgroundImage = `url(${pictureElement.querySelector('img').src})`;
+  const outDiv = document.createElement('div');
+  outDiv.classList.add('outer');
+  const contentDiv = document.createElement('div');
+  contentDiv.classList.add('content');
+  outDiv.append(contentDiv);
+  const colWithCtaDiv = document.createElement('div');
+  colWithCtaDiv.classList.add('col', 'with-cta');
+  contentDiv.append(colWithCtaDiv);
+  const textDiv = document.createElement('div');
+  textDiv.classList.add('text');
+  colWithCtaDiv.append(textDiv);
+  const h2 = document.createElement('h2');
+  h2.classList.add('title');
+  h2.textContent = 'About Aldveron';
+  textDiv.append(h2);
+  const p = document.createElement('p');
+  const span = document.createElement('span');
+  textDiv.append(p);
+  p.append(span);
+  const text1 = block.querySelector('div > div + div > p + p');
+  span.textContent = text1.textContent;
+  const p2 = document.createElement('p');
+  p2.classList.add('actions');
+  const anchor = document.createElement('a');
+  anchor.classList.add('button');
+  anchor.setAttribute('href', '/about-us');
+  anchor.textContent = 'Learn More';
+  p2.append(anchor);
+  textDiv.append(p2);
+  moduleDiv.append(outDiv);
+  block.textContent = '';
+  block.append(moduleDiv)
 }
