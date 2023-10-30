8fa9815f076310f5507a3d2c2e96047c364227fe
TeshuKatepalli <teshu.katepalli@gspann.com>
2023-10-30 15:04:25 +0530

    Removed default hero block

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -19,21 +19,6 @@ import {
 const LCP_BLOCKS = ['forms']; // add your LCP blocks to the list
 
 /**
- * Builds hero block and prepends to main in a new section.
- * @param {Element} main The container element
- */
-function buildHeroBlock(main) {
-  const h1 = main.querySelector('h1');
-  const picture = main.querySelector('picture');
-  // eslint-disable-next-line no-bitwise
-  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
-    const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
-    main.prepend(section);
-  }
-}
-
-/**
  * load fonts.css and set a session storage flag
  */
 async function loadFonts() {
@@ -110,7 +95,6 @@ function buildEmbedBlocks(main) {
  */
 function buildAutoBlocks(main) {
   try {
-    buildHeroBlock(main);
     buildEmbedBlocks(main);
   } catch (error) {
     // eslint-disable-next-line no-console
