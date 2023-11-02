34083e52011e422c563aaf5f2cc224f32562d52d
TeshuKatepalli <teshu.katepalli@gspann.com>
2023-11-02 08:18:29 +0530

    Updated the hero component to get the performance

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,20 +1,23 @@
 export default function decorateBlock(block) {
-  const bgImage = block.children[0].querySelector('img');
+  const bgImage = block.querySelector('img');
 
-  const heroContainer = document.createElement('div');
-  heroContainer.className = 'bg-primary';
+  if (bgImage) {
+    const heroContainer = document.createElement('div');
+    heroContainer.className = 'bg-primary';
+    heroContainer.style.backgroundImage = `url(${bgImage.src})`;
+    heroContainer.style.backgroundSize = 'cover';
+    heroContainer.style.height = '400px'; // Set a fixed height to prevent layout shift
 
-  // Set background image using CSS property
-  heroContainer.style.backgroundImage = `url(${bgImage.src})`;
-  heroContainer.style.backgroundSize = 'cover'; // Adjust as needed
+    block.innerHTML = ''; // Clear the block content before appending
 
-  if (block.children.length > 1) {
-    const additionalContent = block.children[1]; // Change the index if needed
-    if (additionalContent) {
-      additionalContent.classList.add('outer', 'hero-content');
-      heroContainer.appendChild(additionalContent);
+    if (block.children.length > 1) {
+      const additionalContent = block.children[1];
+      if (additionalContent) {
+        additionalContent.classList.add('outer', 'hero-content');
+        heroContainer.appendChild(additionalContent);
+      }
     }
+
+    block.appendChild(heroContainer);
   }
-  block.innerText = '';
-  block.appendChild(heroContainer);
 }
