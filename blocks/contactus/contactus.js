48caa65fcd525bad94c053e5559a8d9ae02cdd1b
Gowtami <gsp24112@adobe.com>
2023-11-09 16:55:12 +0530

    fixed lint issues

--- blocks/contactus/contactus.js
+++ blocks/contactus/contactus.js
@@ -19,8 +19,8 @@ export default function decorate() {
     } else if (i === 1) {
       innerElements += pTags[i].outerHTML;
     } else if (i === 3) {
-      let contactNumber = pTags[2].innerHTML.trim();
-      let contactNumberReplace = contactNumber.replace(/[()-\s]/g,'');
+      const contactNumber = pTags[2].innerHTML.trim();
+      const contactNumberReplace = contactNumber.replace(/[()-\s]/g, '');
       phone = pTags[2].outerHTML.replace(/<p>/g, `<a class='phone' href="tel:${contactNumberReplace}">`);
       phone.replace(/<\/p>/g, '</a>');
       pTags[i].innerHTML = `<br><div class="buttons"> ${phone + pTags[i].innerHTML} </div>`;
