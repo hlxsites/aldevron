342afa8b06bdd677077d8a1c5cde0242055e0be5
Swapnamayee Sahoo <swapnamayee.sahoo@MACPRO479.local>
2023-11-22 15:05:40 +0530

    159: height and width issue fixed

--- blocks/vimeo/vimeo.js
+++ blocks/vimeo/vimeo.js
@@ -2,8 +2,8 @@ export default function decorate(block) {
   const anchorLink = block.querySelector('a');
   const ifrm = document.createElement('iframe');
   ifrm.setAttribute('src', anchorLink);
-  ifrm.style.width = '800px';
-  ifrm.style.height = '600px';
+  ifrm.style.width = '980px';
+  ifrm.style.height = '552px';
   ifrm.style.frameBorder = '0';
   ifrm.style.allow = 'autoplay; fullscreen; picture-in-picture';
   ifrm.setAttribute('allowFullScreen', '');
