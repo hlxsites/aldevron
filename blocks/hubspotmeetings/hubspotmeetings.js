450d8bd4c656d64b37326737e368fe4c02e38ae9
TeshuKatepalli <teshu.katepalli@gspann.com>
2023-11-29 05:12:58 +0530

    Updated a little delay on form load

--- blocks/hubspotmeetings/hubspotmeetings.js
+++ blocks/hubspotmeetings/hubspotmeetings.js
@@ -1,15 +1,25 @@
 function loadHSScript() {
-    const scriptTag = document.createElement('script');
-    scriptTag.src = 'https://static.hsappstatic.net/MeetingsEmbed/ex/MeetingsEmbedCode.js';
-    document.head.prepend(scriptTag);
+    if (!document.querySelector('script[src="https://static.hsappstatic.net/MeetingsEmbed/ex/MeetingsEmbedCode.js"]')) {
+        const scriptTag = document.createElement('script');
+        scriptTag.src = 'https://static.hsappstatic.net/MeetingsEmbed/ex/MeetingsEmbedCode.js';
+        scriptTag.async = true;
+        document.head.appendChild(scriptTag);
+    }
 }
 
 export default function decorate(block) {
-    loadHSScript();
     const hsCalendarBlock = document.createElement('div');
     hsCalendarBlock.id = 'hs_script';
+
+    // Specify a fixed size or use CSS to define dimensions to prevent layout shifts
+    hsCalendarBlock.style.width = '100%'; // Adjust these values based on the expected size
+    hsCalendarBlock.style.minHeight = '400px'; // Adjust these values based on the expected size
+
     let blockFromHubSpot = '<div class="meetings-iframe-container" data-src="https://app.hubspot.com/meetings/tom-lynch?embed=true">&nbsp;</div>';
     hsCalendarBlock.innerHTML = blockFromHubSpot;
+    block.innerText = '';
     block.appendChild(hsCalendarBlock);
-  }
-  
+    setTimeout(() => {
+        loadHSScript();
+    }, 3000);
+}
