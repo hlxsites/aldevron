de2f25c061e279de139b685a43b46a87b77184f8
vinaykumargujja <vinay.gujja@gspann.com>
2023-11-29 17:05:02 +0530

    fixed mouse issue

--- blocks/timeline-slide/timeline-slide.js
+++ blocks/timeline-slide/timeline-slide.js
@@ -8,6 +8,7 @@ export default function decorate(block) {
   const prevButton = div({ class: 'button-block' }, div({ class: 'button-prev' }));
   const buttonContainer = div({ class: 'button-container' });
   let activeYearIndex = 0;
+  let touchStartX = 0;
 
   buttonContainer.appendChild(prevButton);
   buttonContainer.appendChild(nextButton);
@@ -36,6 +37,47 @@ export default function decorate(block) {
       setTimeout(() => {
         content.style.transform = 'translateX(0)';
       }, 50); // Delay to ensure the transition effect is applied
+    }
+  }
+
+  function slideToRightMouse() {
+    // Calculate the next index
+    const nextIndex = activeYearIndex + 1;
+
+    if (nextIndex < yearSlider.children.length) {
+      // Move to the next year and update the active index
+      yearSlider.children[nextIndex].click();
+      // yearSlider.children[activeYearIndex].click();
+      activeYearIndex = nextIndex;
+      // Always show the prev button when moving forward
+      prevButton.classList.remove('hide');
+      translateYearSlider();
+    }
+
+    // Hide the next button if we reach the end
+    if (nextIndex === yearSlider.children.length - 1) {
+      nextButton.classList.add('hide');
+    }
+  }
+
+  function slideToLeftMouse() {
+    const prevIndex = activeYearIndex - 1;
+
+    if (prevIndex >= 0) {
+      // Move to the previous year and update the active index
+      yearSlider.children[prevIndex].click();
+      activeYearIndex = prevIndex;
+
+      // Always show the next button when moving backward
+      nextButton.classList.remove('hide');
+
+      // Translate the year slider
+      translateYearSlider();
+    }
+
+    // Hide the prev button if we reach the beginning
+    if (prevIndex === 0) {
+      prevButton.classList.add('hide');
     }
   }
 
@@ -83,12 +125,26 @@ export default function decorate(block) {
     if (child.children.length >= 2) {
       const yearChild = child.children[0].cloneNode(true);
       yearChild.classList.add('year');
-      yearChild.style.width = '130px';
       yearSlider.appendChild(yearChild);
       const contentChild = child.children[1].cloneNode(true);
       contentChild.classList.add('year-content');
       contentChild.id = `content-${yearChild.innerText}`;
       contentSlider.appendChild(contentChild);
+    }
+  });
+
+  block.addEventListener('touchstart', (e) => {
+    touchStartX = e.touches[0].clientX;
+  });
+
+  block.addEventListener('touchend', (e) => {
+    const touchEndX = e.changedTouches[0].clientX;
+    const deltaX = touchEndX - touchStartX;
+    // If the touch ends to the right of the starting position, move to the previous slide
+    if (deltaX < 0) {
+      slideToRightMouse();
+    } else if (deltaX > 0) {
+      slideToLeftMouse();
     }
   });
 
@@ -102,6 +158,16 @@ export default function decorate(block) {
   [...yearSlider.children].forEach((year, index) => {
     year.addEventListener('click', () => {
       clearActiveClasses();
+
+      // disable previous arrow button based on first slide.
+      if (index === 0 && !prevButton.classList.contains('hide')) {
+        prevButton.classList.add('hide');
+      } else { prevButton.classList.remove('hide'); }
+
+      // disable next arrow button based on last slide.
+      if (index === yearSlider.children.length - 1 && !nextButton.classList.contains('hide')) {
+        nextButton.classList.add('hide');
+      } else { nextButton.classList.remove('hide'); }
 
       // Check direction for slide effect
       const direction = index > activeYearIndex ? 'left' : 'right';
