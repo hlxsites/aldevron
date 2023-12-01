fdb4d2673e99f6f3765b2102939062ecf41a029e
vinaykumargujja <vinay.gujja@gspann.com>
2023-12-01 13:30:59 +0530

    children updated

--- blocks/herobanner/herobanner.js
+++ blocks/herobanner/herobanner.js
@@ -3,7 +3,7 @@ export default function decorate(block) {
   const moduleDiv = document.createElement('div');
   moduleDiv.classList.add('module', 'mmg-rich-columns', 'padding-all', 'custom-bg', 'bg', 'wide-section', 'split-bg', 'style-standard');
   moduleDiv.style.backgroundImage = `url(${imageContaner.querySelector('img').src})`;
-  const contentContainer = block.children[1];
+  const contentContainer = block.children[0].children[1];
   const outDiv = document.createElement('div');
   outDiv.classList.add('outer');
   const contentDiv = document.createElement('div');
