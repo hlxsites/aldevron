bc4a1d780c273fbdfc4db1f2e99c84ae8d01ab9c
Ankit Gupta <ankit.gupta@gspann.com>
2024-05-13 19:05:30 +0530

    code update for debounce

--- scripts/aem.js
+++ scripts/aem.js
@@ -860,6 +860,14 @@ async function waitForLCP(lcpBlocks) {
   });
 }
 
+function debounce(func, timeout = 300) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => { func.apply(this, args); }, timeout);
+  };
+}
+
 init();
 
 export {
@@ -897,11 +905,3 @@ export {
   getFormMeetingConfig,
   debounce,
 };
-
-export function debounce(func, timeout = 300) {
-  let timer;
-  return (...args) => {
-    clearTimeout(timer);
-    timer = setTimeout(() => { func.apply(this, args); }, timeout);
-  };
-}
