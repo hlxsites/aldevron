992ed6cc17c0967eb957a50f08e4ca6291a0a970
Dave Nichols <dave.nichols@dhlifesciences.com>
2024-12-02 15:05:09 +0000

    Fixed lint issues

--- blocks/dave/dave.js
+++ blocks/dave/dave.js
@@ -1,13 +1,13 @@
 export default function decorate(block) {
-const quoteDiv = block.querySelector('div:last-of-type');
-const dave = document.createElement('dave');
-dave.innerHTML = `<strong>${quoteDiv.innerHTML}</strong>`;
-quoteDiv.replaceWith(dave);
+  const quoteDiv = block.querySelector('div:last-of-type');
+  const dave = document.createElement('dave');
+  dave.innerHTML = `<strong>${quoteDiv.innerHTML}</strong>`;
+  quoteDiv.replaceWith(dave);
 
-const authorDiv = block.querySelector('div:nth-child(2)');
-if (authorDiv) {
+  const authorDiv = block.querySelector('div:nth-child(2)');
+  if (authorDiv) {
     const author = document.createElement('p');
     author.innerHTML = `<i>${authorDiv.innerHTML}</i>`;
     authorDiv.replaceWith(author);
-}
+  }
 }
