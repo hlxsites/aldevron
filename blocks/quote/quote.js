0ef27977a24c2270d4f8a4b0dab632a4fc94692e
venkatesh-vanaboina <venkatesh.vanaboina@gspann.com>
2025-03-28 21:05:37 +0530

    venkat quote block update
    venkat quote block update

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,4 +1,12 @@
+import { div } from '../../scripts/dom-builder.js';
+
 export default function decorate(block) {
+// Create a container div for the entire block
+  const container = div({class: 'quote-container'})
+  const bgColorDiv = div({class: 'background-color'})
+  container.appendChild(bgColorDiv); 
+  block.prepend(container);
+
   // Loop through all the rows in the block
   [...block.children].forEach((row) => {
       if (row.children.length > 1) {
@@ -7,19 +15,15 @@ export default function decorate(block) {
          // Validate if the text content of the second column is a valid hex code
           const isValidHex = /^#[0-9A-Fa-f]{6}$/.test(colorCode);
           if (isValidHex) {
-              block.style.backgroundColor = colorCode;
+            bgColorDiv.style.backgroundColor = colorCode;
           } else {
-              block.style.backgroundColor = '#f5f5f5';
+            bgColorDiv.style.backgroundColor = '#f5f5f5';
           }            
           const row1Column1 = row.children[0];
+          row1Column1.classList.add('title');
           const h2Element = row1Column1.querySelector('h2');
-          if (h2Element) {
-              h2Element.style.color = 'white';
-              h2Element.classList.add('quote-title');
-              h2Element.style.borderLeft = 'unset';
-              h2Element.style.padding = '0 0 0 100px';  
-          }
             row1Column2.style.display = 'none';
+            container.appendChild(row1Column1);
       }
   });
 }
