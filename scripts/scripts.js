21268f7765b2ed2d7e20f33b89d49f13a2c26056
davenichols-DHLS <124819426+davenichols-DHLS@users.noreply.github.com>
2025-06-11 16:48:01 +0100

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -430,6 +430,25 @@ function correctUTMFlow() {
   }
 }
 
+// SalesForce MCP - start
+
+function loadEvergageScript() {
+  const script = document.createElement('script');
+  if (window.location.host === 'www.aldevron.com') {
+    script.src = 'https://cdn.evgnet.com/beacon/v55685555553mx3rf3h3n3n3i091550196/aldevron_prod/scripts/evergage.min.js';
+  } else {
+    script.src = 'https://cdn.evgnet.com/beacon/v55685555553mx3rf3h3n3n3i091550196/aldevron_staging/scripts/evergage.min.js';
+  }
+  script.onload = function onEvergageLoad() {
+  };
+  script.onerror = function onEvergageError() {
+  };
+  document.head.appendChild(script);
+}
+loadEvergageScript();
+
+// SalesForce MCP - end
+
 function getCookie(e) {
   let t = decodeURIComponent(
     document.cookie.replace(
