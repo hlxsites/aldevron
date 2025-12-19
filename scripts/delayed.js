ed5e4110fa95f35b1e8f75412d7114832e245e81
Balwant Singh <balwant.singh@nvish.com>
2025-12-19 16:56:17 +0530

    check OT

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,7 +5,36 @@ import {
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
-
+// google tag manager -start
+function loadGTM() {
+  const scriptTag = document.createElement('script');
+  scriptTag.innerHTML = `
+        let gtmId = 'GTM-MLWV3QQ';
+        // googleTagManager
+        (function (w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start':
+                    new Date().getTime(), event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', gtmId);
+        `;
+  document.head.prepend(scriptTag);
+  const noScriptTag = document.createElement('noscript');
+  noScriptTag.innerHTML = `
+  <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MLWV3QQ"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe>
+  `;
+  document.body.prepend(noScriptTag);
+}
+// google tag manager -end
+// loadGTM();
 // Fathom Analytics Code
 const attrsFa = JSON.parse('{"data-site": "TSVSBXOE"}');
 loadScript('https://cdn.usefathom.com/script.js', attrsFa);
@@ -20,7 +49,7 @@ function loadHsScript() {
   hsScriptEl.src = '//js.hs-scripts.com/1769030.js';
   document.querySelector('head').append(hsScriptEl);
 }
-loadHsScript();
+// loadHsScript();
 // HubSpot Form Code
 function loadHubSpot() {
   const hsScriptEl = document.createElement('script');
