268a3c0835ac0b68629242c6d8444c3762de68ad
Balwant Singh <balwant.singh@nvish.com>
2026-05-13 16:21:36 +0530

    GTM test

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -81,8 +81,8 @@ window.OptanonWrapper = function OptanonWrapper() {
   // Analytics OR Marketing hubspot C0002
   if (groups.includes('C0002')) {
     console.log('[Consent] hubspot C0002 → loading');
-    //loadHsScript();
-    //loadEvergageScript();
+    loadHsScript();
+    loadEvergageScript();
   } else {
     // console.log('[Consent] Not granted → blocked');
   }
