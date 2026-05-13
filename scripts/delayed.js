4c5c2b41b1d61c0cc896ec40c447108b2c34d2aa
Balwant Singh <balwant.singh@nvish.com>
2026-05-13 17:28:13 +0530

    GTM test

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -40,7 +40,7 @@ if (
   && !document.location.hostname.includes('.hlx')
 ) {
   console.log('GTM → loading');
-  //loadGTM();
+  loadGTM();
 }
 // Fathom Analytics Code
 const attrsFa = JSON.parse('{"data-site": "TSVSBXOE"}');
@@ -103,7 +103,7 @@ function loadHubSpot() {
 
 // only load this if there is a form block
 if (isForm()) {
-  //loadHubSpot();
+  loadHubSpot();
 }
 
 if (getFormMeetingConfig()) {
